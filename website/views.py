09fa9d150ea11aae70f61546580225554bdbb914
Th3Heretic <d.snow7324@student.leedsbeckett.ac.uk>
2024-10-18 17:52:29 +0100

    .

--- website/views.py
+++ website/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html')  # Renders the index.html template
+    return render(request, 'Landing.html')  # Renders the Landing.html template
